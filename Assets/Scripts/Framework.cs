b9864695f8ff2e63422c69a4f99672383f6644a5
Erol KOCAMAN <kocamanerol@gmail.com>
2014-04-15 15:54:00 +0300

    Initial framework commit

---
+++ Assets/Scripts/Framework.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Framework : MonoBehaviour {
+
+	public static float int COUNTER = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
